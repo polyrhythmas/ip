783f7c111307b40bf270bbbb0dda1f8829a90d88
tristan <polyrhythmas@gmail.com>
2025-08-22 00:47:03 +0800

    Added horizontal line function

--- src/main/java/Diablo.java
+++ src/main/java/Diablo.java
@@ -2,4 +2,15 @@ public class Diablo {
     public static void main(String[] args) {
 
     }
+
+    private static void printHorizontalLine() {
+        char line = '-';
+        int lineLength = 50;
+
+        for (int i = 0; i < lineLength; i++) {
+            System.out.print(line);
+        }
+
+        System.out.println();
+    }
 }
