55b8331ed5a5a4015a48e3bd0d987878496366d2
tristan <polyrhythmas@gmail.com>
2025-08-22 02:45:10 +0800

    Changed toString for Deadline and Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,7 +10,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 
 }
