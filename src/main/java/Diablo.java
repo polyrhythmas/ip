46f37c507f056a6bb49784cf25ecc3d154ee530a
tristan <polyrhythmas@gmail.com>
2025-08-22 00:55:42 +0800

    Added greetings

--- src/main/java/Diablo.java
+++ src/main/java/Diablo.java
@@ -1,6 +1,14 @@
 public class Diablo {
     public static void main(String[] args) {
-
+        String botName = "Diablo";
+        String greeting = "Hello! I'm " + botName + "\nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        
+        printHorizontalLine();
+        System.out.println(greeting);
+        printHorizontalLine();
+        System.out.println(exit);
+        printHorizontalLine();
     }
 
     private static void printHorizontalLine() {
