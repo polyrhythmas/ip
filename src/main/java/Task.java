fdd9736461b84044f36ee43da5b2dd44ccbf6425
tristan <polyrhythmas@gmail.com>
2025-08-22 01:33:48 +0800

    Added Task class functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,17 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void complete() {
+        this.isDone = true;
+    }
 }
