8a869a1287d93d579420d5da73f649b318976e96
tristan <polyrhythmas@gmail.com>
2025-08-29 02:24:12 +0800

    Add edge case for wrong date format in Parser class

--- src/main/java/diablo/ui/Parser.java
+++ src/main/java/diablo/ui/Parser.java
@@ -1,5 +1,10 @@
 package diablo.ui;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Parser {
     public static String[] parse(String input) {
         String firstWord = input.split(" ")[0];
@@ -25,6 +30,11 @@ public class Parser {
             } else {
                 String description = input.substring(9, startBy - 1);
                 String day = input.substring(startBy + 4);
+                try {
+                    LocalDate date = LocalDate.parse(day);
+                } catch (DateTimeParseException e) {
+                    return new String[] {"deadline", "-1", "Please write the date in the format yyyy-mm-dd"};
+                }
                 return new String[] {"deadline", description, day};
             }
         }
@@ -47,6 +57,12 @@ public class Parser {
                     String description = input.substring(6, startFrom - 1);
                     String from = input.substring(startFrom + 6, startTo - 1);
                     String to = input.substring(startTo + 4);
+                    try {
+                        LocalDate dateFrom = LocalDate.parse(from);
+                        LocalDate dateTo = LocalDate.parse(to);
+                    } catch (DateTimeParseException e) {
+                        return new String[] {"event", "-1", "Please write the dates in the format yyyy-mm-dd"};
+                    }
                     return new String[] {"event", description, from, to};
                 }
             }
