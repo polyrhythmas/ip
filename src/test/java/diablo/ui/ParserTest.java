0bfa64ebf8d7c453ed3521abc62d50b54cdcb60d
tristan <polyrhythmas@gmail.com>
2025-09-12 02:11:48 +0800

    Improve code quality

--- src/test/java/diablo/ui/ParserTest.java
+++ src/test/java/diablo/ui/ParserTest.java
@@ -7,19 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTest {
     @Test
     public void parse_deadlineWithNoDate_errorMessage() {
-        String[] output = Parser.parse("deadline i wanna go home");
+        Parser parser = new Parser();
+        String[] output = parser.parse("deadline i wanna go home");
         assertEquals("Please indicate a deadline!", output[2]);
     }
 
     @Test
     public void parse_deadlineWithWrongDateFormat_errorMessage() {
-        String[] output = Parser.parse("deadline i wanna go home /by 19 January 2024");
+        Parser parser = new Parser();
+        String[] output = parser.parse("deadline i wanna go home /by 19 January 2024");
         assertEquals("Please write the date in the format yyyy-mm-dd", output[2]);
     }
 
     @Test
     public void parse_deadlineWithMissingDate_errorMessage() {
-        String[] output = Parser.parse("deadline i wanna go home /by ");
+        Parser parser = new Parser();
+        String[] output = parser.parse("deadline i wanna go home /by ");
         assertEquals("Please write the date in the format yyyy-mm-dd", output[2]);
     }
 }
